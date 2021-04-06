3346ff71381e0f4f9529d0d59038b0e3ccb6e1b3
epaillas <epaillas@astro.puc.cl>
2021-04-06 15:47:40 -0400

    Debug

--- contrast/box/fortran/mean_radial_velocity_vs_r.f90
+++ contrast/box/fortran/mean_radial_velocity_vs_r.f90
@@ -176,7 +176,7 @@ program mean_radial_velocity_vs_r
   !$OMP ix, iy, iz, ix2, iy2, iz2, disx, disy, disz, dis, dis2, rind, &
   !$OMP vel, vr, velx, vely, velz)
   do i = 1, ncentres
-
+    write(*,*) i
     ipx = int(centres(1, i) / rgrid + 1.)
     ipy = int(centres(2, i) / rgrid + 1.)
     ipz = int(centres(3, i) / rgrid + 1.)
