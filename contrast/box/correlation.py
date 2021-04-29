9d71be28e4555621df18d49197a9410ab883739d
epaillas <epaillas@astro.puc.cl>
2021-04-28 21:42:43 -0400

    Add LOS velocity dispersion profile

--- contrast/box/correlation.py
+++ contrast/box/correlation.py
@@ -202,3 +202,73 @@ def mean_radial_velocity_vs_r(
   corr = data[:,1]
 
   return r, corr
+
+
+def std_los_velocity_vs_rmu(
+  data_filename1, output_filename,
+  box_size, dim1_min, dim1_max,
+  dim1_nbin, ngrid, data_filename2=None, 
+  nthreads=1, dim2_min=-1, dim2_max=1,
+  dim2_nbin=80
+):
+  '''
+  Velocity dispersion profile as a function of radial
+  and LOS angle separation.
+
+  Input arguments:
+
+  data_filename1: name of text file containing first
+  set of tracers, where the first three columns correspond
+  to the positions in cartesian coordinates.
+
+  output_filename: name of text file where the output 
+  is going to be stored.
+
+  box_size: size of the simulation box.
+
+  dim1_min: minimum pair distance.
+
+  dim1_max: maximum pair distance.
+
+  dim1_nbin: number of radial bins.
+
+  ngrid: number of cells in which to divide the simulation box 
+  to speed up calculation. It has to be a divisor of box_size 
+  (e.g. 100 for 1000 Mpc/h box, or 128 for a 1024 Mpc/h box).
+
+  data_filename2 (optional): name of text file containing second 
+  set of points for a cross-correlation function.
+
+  nthreads (optional): number of threads to use while running the 
+  algorithm.
+
+  dim2_min: (optional) minimum mu separation.
+
+  dim2_max: (optional) maximum mu separation .
+
+  dim2_nbin: (optional) number of mu bins.
+  '''
+
+  # check if files exist
+  if not path.isfile(data_filename1):
+    raise FileNotFoundError('{} does not exist.'.format(data_filename1))
+
+  if data_filename2 == None:
+    data_filename2 = data_filename1
+
+  binpath = path.join(path.dirname(__file__),
+  'bin', 'std_los_velocity_vs_rmu.exe')
+
+  cmd = [
+    binpath, data_filename1, data_filename2,
+    output_filename, str(box_size), str(dim1_min),
+    str(dim1_max), str(dim1_nbin), str(dim2_min),
+    str(dim2_max), str(dim2_nbin), str(ngrid),
+    str(nthreads)
+  ]
+
+  log_filename = '{}.log'.format(output_filename)
+  log = open(log_filename, 'w+')
+  subprocess.call(cmd, stdout=log, stderr=log)
+
+  return
