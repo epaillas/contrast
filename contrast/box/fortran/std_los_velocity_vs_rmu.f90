04921bec9da2b0365295168d2b224fe579f1965a
epaillas <epaillas@astro.puc.cl>
2021-04-28 23:44:11 -0400

    Assume LOS is the z axis

--- contrast/box/fortran/std_los_velocity_vs_rmu.f90
+++ contrast/box/fortran/std_los_velocity_vs_rmu.f90
@@ -186,8 +186,6 @@ program mean_radial_velocity_vs_r
                 mu = (disx * comx + disy * comy + disz * comz) &
                 & / (dis * sqrt(comx * comx + comy * comy + comz * comz))
 
-                write(*,*) mu
-        
                 rind = int((dis - dim1_min) * irwidth + 1)
                 muind = int((mu - dim2_min) * imuwidth + 1)
                 D1D2(rind, muind) = D1D2(rind, muind) + weight1(i) * weight2(ii)
