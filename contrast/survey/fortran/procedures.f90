995d9d4e8da3c1f229ee53fd2fd5ebd91247d464
epaillas <epaillasv@gmail.com>
2021-05-31 23:34:38 -0400

    Add 2d correlation function

--- contrast/survey/fortran/procedures.f90
+++ contrast/survey/fortran/procedures.f90
@@ -41,6 +41,41 @@ contains
 
   end subroutine linked_list
 
+  subroutine linked_list_2d(pos, ngrid, gridmin, gridmax, ll, lirst, rgrid)
+    implicit none
+    integer*8 :: ng
+    integer*8 :: i, ipx, ipy, ipz
+    integer*8, intent(in) :: ngrid
+    real*8, intent(out) :: rgrid
+    real*8, intent(in) :: gridmin, gridmax
+    real*8, dimension(:,:), intent(in) :: pos
+    integer*8, dimension(:,:), intent(out) :: lirst
+    integer*8, dimension(:), intent(out) :: ll
+
+    rgrid = (gridmax - gridmin) / ngrid
+
+    ng = size(pos, dim=2)
+    lirst = 0
+    ll = 0
+    do i = 1, ng
+      ipx = int((pos(1, i) - gridmin) / rgrid + 1.)
+      ipy = int((pos(2, i) - gridmin) / rgrid + 1.)
+      if(ipx.gt.0.and.ipx.le.ngrid.and.ipy.gt.0.and.ipy.le.ngrid)&
+         & lirst(ipx, ipy) = i
+    end do
+
+    do i = 1, ng
+      ipx = int((pos(1, i) - gridmin) / rgrid + 1.)
+      ipy = int((pos(2, i) - gridmin) / rgrid + 1.)
+
+      if (ipx.gt.0.and.ipx.le.ngrid.and.ipy.gt.0.and.ipy.le.ngrid) then
+        ll(lirst(ipx, ipy)) = i
+        lirst(ipx, ipy) = i
+      endif
+    end do
+
+  end subroutine linked_list_2d
+
   subroutine read_unformatted(input_filename, data, weight, np)
     implicit none
     integer*8 :: nrows, ncols
